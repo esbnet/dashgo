adb5365e2758f96ecd60f6f8f537a5d9630e7658
Edmilson Soares <esbnet@gmail.com>
2022-05-13 18:46:05 -0300

    instal and  conf chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+	styles: {
+		global: {
+			body: {
+				bg: "yellow.900",
+				color: "yellow.100",
+			},
+		},
+	},
+});
